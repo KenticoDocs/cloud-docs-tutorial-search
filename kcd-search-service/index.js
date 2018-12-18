3518db2ec1af241dcbb70d55583c9c3f1a7a468c
Juraj Bielik <jurajb@kentico.com>
2018-12-18 17:17:59 +0100

    KA-420 Refactor index.js and event validation

--- kcd-search-service/index.js
+++ kcd-search-service/index.js
@@ -1,27 +1,38 @@
 const getCodenamesOfItems = require('./utils/codenamesExtractor');
-const validateEvent = require('./eventValidator');
 const indexers = require('./searchIndexers');
 
+const SubscriptionValidationEvent = "Microsoft.EventGrid.SubscriptionValidationEvent";
+
+function isNonEmptyKenticoEvent(event) {
+  return event.eventType === 'kentico' && event.data.items;
+}
+
 module.exports = (context, event) => {
-  const parsedReq = validateEvent(context, event);
+  const singleEvent = event.body[0];
 
-  if (parsedReq[0].subject && parsedReq[0].data) {
-    const subject = parsedReq[0].subject;
-    const items = parsedReq[0].data.items;
+  if (singleEvent.eventType === SubscriptionValidationEvent) {
+    context.res = {
+      body: {
+        validationResponse: singleEvent.data.validationCode
+      }
+    };
 
-    if (subject === 'initialize') {
+  } else if (singleEvent.subject && singleEvent.data) {
+
+    if (singleEvent.subject === 'initialize') {
       indexers.reindexAllArticles();
-    }
 
-    if (items && (subject === 'publish' || subject === 'unpublish')) {
-      const codenames = getCodenamesOfItems(items, 'article');
+    } else if (isNonEmptyKenticoEvent(singleEvent) && (subject === 'publish' || subject === 'unpublish')) {
+      const codenames = getCodenamesOfItems(singleEvent.data.items, 'article');
       indexers.indexSpecificArticles(codenames);
-    }
 
-    if (items && subject === 'archive') {
-      const codenames = getCodenamesOfItems(items, 'article');
+    } else if (isNonEmptyKenticoEvent(singleEvent) && subject === 'archive') {
+
+      const codenames = getCodenamesOfItems(singleEvent.data.items, 'article');
       indexers.deleteIndexedArticles(codenames);
       indexers.indexSpecificArticles(codenames);
     }
   }
+
+  context.done();
 };
