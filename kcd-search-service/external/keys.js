04f13c3b09f96981bb1cd9357c29dab72ed8f509
jirik <jirik@kentico.com>
2018-12-20 08:41:33 +0100

    KA-446 - rename app settings

--- kcd-search-service/external/keys.js
+++ kcd-search-service/external/keys.js
@@ -1,8 +1,8 @@
-const kenticoProjectId = process.env["DOCS_PROJECT_ID"];
+const kenticoProjectId = process.env["KC.ProjectId"];
 
-const searchAppId = process.env["SEARCH_APP_ID"];
-const adminApiKey = process.env["SEARCH_ADMIN_API_KEY"];
-const index = process.env["SEARCH_INDEX"];
+const searchAppId = process.env["Search.AppId"];
+const adminApiKey = process.env["Search.ApiKey"];
+const index = process.env["Search.IndexName"];
 
 module.exports = {
     kenticoProjectId,
