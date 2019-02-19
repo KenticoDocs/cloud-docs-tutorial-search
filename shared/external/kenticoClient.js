90992e316e5cc5853d44ca222d834e8da3d7d8b4
Juraj Bielik <jurajb@kentico.com>
2019-02-19 12:36:50 +0100

    KA-468 Get rid of singleton external clients

--- shared/external/kenticoClient.js
+++ shared/external/kenticoClient.js
@@ -2,23 +2,17 @@ const KenticoCloud = require('kentico-cloud-delivery');
 const Article = require('../models/article');
 const { keys } = require('./configuration');
 
-let kenticoClient;
-
 const typeResolvers = [
     new KenticoCloud.TypeResolver('article', () => new Article()),
 ];
 
 function getKenticoClient() {
-    if (kenticoClient === undefined) {
-        kenticoClient = new KenticoCloud.DeliveryClient({
-            projectId: keys.kenticoProjectId,
-            enableSecuredMode: true,
-            securedApiKey: keys.securedApiKey,
-            typeResolvers,
-        });
-    }
-
-    return kenticoClient;
+    return new KenticoCloud.DeliveryClient({
+        projectId: keys.kenticoProjectId,
+        enableSecuredMode: true,
+        securedApiKey: keys.securedApiKey,
+        typeResolvers,
+    });
 }
 
 module.exports = getKenticoClient;
