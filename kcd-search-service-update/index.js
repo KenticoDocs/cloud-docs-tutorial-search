d74036b93f093e8beb1770a4c76dfdd7f71878da
Juraj Bielik <jurajb@kentico.com>
2019-02-20 13:03:44 +0100

    KA-506 Fix integration test support

--- kcd-search-service-update/index.js
+++ kcd-search-service-update/index.js
@@ -2,20 +2,22 @@ const getCodenamesOfItems = require('../shared/utils/codenamesExtractor');
 const indexers = require('../shared/searchIndexers');
 const { setupConfiguration } = require('../shared/external/configuration');
 
-const validEventSubjects = ['publish', 'upsert', 'unpublish', 'archive'];
+const validReindexOperations = ['publish', 'upsert', 'restore_publish'];
+const validDeleteOperations = ['unpublish', 'archive'];
+const validOperations = validReindexOperations.concat(validDeleteOperations);
 
 function validateEvent(event) {
     return event.eventType === 'kentico-cloud' &&
-        event.data.items &&
-        validEventSubjects.includes(event.subject);
+        event.data.webhook.items &&
+        validOperations.includes(event.subject);
 }
 
 module.exports = async (context, eventGridEvent) => {
     if (validateEvent(eventGridEvent)) {
-        setupConfiguration(eventGridEvent.test);
-        const codenames = getCodenamesOfItems(eventGridEvent.data.items, 'article');
+        setupConfiguration(eventGridEvent.data.test);
+        const codenames = getCodenamesOfItems(eventGridEvent.data.webhook.items, 'article');
 
-        if (eventGridEvent.subject === 'publish' || eventGridEvent.subject === 'upsert') {
+        if (validReindexOperations.includes(eventGridEvent.subject)) {
             await indexers.reindexSpecificArticles(codenames);
         } else {
             indexers.deleteIndexedArticles(codenames);
