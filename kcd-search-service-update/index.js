592f357268995fd9b36d3eb7f883655e34995536
jirik <jirik@kentico.com>
2019-02-26 10:24:20 +0100

    KA-511 Code simplification, renames, constant reuse

--- kcd-search-service-update/index.js
+++ kcd-search-service-update/index.js
@@ -1,12 +1,11 @@
 const getCodenamesOfItems = require('../shared/utils/codenamesExtractor');
 const indexers = require('../shared/searchIndexers');
 const { setupConfiguration } = require('../shared/external/configuration');
-
-const VALID_REINDEX_OPERATIONS = ['publish', 'upsert', 'restore_publish'];
-const VALID_DELETE_OPERATIONS = ['unpublish', 'archive'];
-const VALID_OPERATIONS = VALID_REINDEX_OPERATIONS.concat(VALID_DELETE_OPERATIONS);
-
-const CONTENT_ITEMS_TO_INDEX = ['article', 'scenario'];
+const {
+    VALID_OPERATIONS,
+    VALID_REINDEX_OPERATIONS,
+    CONTENT_TYPES_TO_INDEX
+} = require('../shared/external/constants');
 
 function validateEvent(event) {
     return event.eventType === 'kentico-cloud' &&
@@ -17,7 +16,7 @@ function validateEvent(event) {
 module.exports = async (context, eventGridEvent) => {
     if (validateEvent(eventGridEvent)) {
         setupConfiguration(eventGridEvent.data.test);
-        const codenames = getCodenamesOfItems(eventGridEvent.data.webhook.items, CONTENT_ITEMS_TO_INDEX);
+        const codenames = getCodenamesOfItems(eventGridEvent.data.webhook.items, CONTENT_TYPES_TO_INDEX);
 
         if (VALID_REINDEX_OPERATIONS.includes(eventGridEvent.subject)) {
             await indexers.reindexSpecificItems(codenames);
